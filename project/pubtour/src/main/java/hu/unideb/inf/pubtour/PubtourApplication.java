88d4fee2432f68759b4c6d1bac6c59247b6afa98
Bence Juhasz <buvesz@mailbox.unideb.hu>
2025-09-25 15:31:11 +0200

    update requirements.md

---
+++ project/pubtour/src/main/java/hu/unideb/inf/pubtour/PubtourApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.pubtour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubtourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubtourApplication.class, args);
+	}
+
+}
